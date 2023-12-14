4300c599caf4310ab20ad87c4f900f42a651bc55
Isaac Han <isaachan233@gmail.com>
2023-12-14 19:40:40 +0900

    Lazy와 Eager의 차이

--- src/main/java/com/example/adminpage/model/entity/User.java
+++ src/main/java/com/example/adminpage/model/entity/User.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -31,4 +32,27 @@ public class User {
     private LocalDateTime updatedAt;
 
     private String updatedBy;
+
+
+    // 1 : N
+    // LAZY = 지연 로딩
+    // oneToMany , manyToMany 의 default
+    // select * from user u1_0 where u1_0.id=?
+    // 필요한 시점에 연관된 데이터를 불러옴
+
+    // EAGER = 즉시 로딩
+    // select * from user u1_0
+    // left join order_detail odl1_0 on u1_0.id=odl1_0.user_id
+    // left join item i1_0 on i1_0.id=odl1_0.item_id
+    // where u1_0.id=?
+    // 연관된 Table을 한 꺼번에 join 시켜서 데이터를 호출함
+
+    // 만약 연관관계가 더 많은 실제 실무 프로젝트에서는
+    // 자원의 낭비나 실제로 호출이 안되는 경우도 있기 때문에
+    // LAZY를 사용한다고 한다.
+
+    // oneToOnt , manyToOne 의 default
+    // mappedBy 는 OrderDetail 클래스안에 user에 매칭 시킨다는 뜻
+    @OneToMany(fetch = FetchType.EAGER, mappedBy = "user")
+    private List<OrderDetail> orderDetailList;
 }
