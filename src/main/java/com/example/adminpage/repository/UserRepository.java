14ba11ef28c4ff383588237810e427541a951f99
Isaac Han <isaachan233@gmail.com>
2023-12-14 19:41:23 +0900

    feat: 기본 CRUD에 다양한 조건 추가

--- src/main/java/com/example/adminpage/repository/UserRepository.java
+++ src/main/java/com/example/adminpage/repository/UserRepository.java
@@ -4,7 +4,15 @@ import com.example.adminpage.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> { // <인자 받아올 엔티티의 형식, pk (primaryKey)의 형식>
+    // select * from user where account = ? << test03, test04
+    Optional<User> findByAccount(String account);
+    // select * from user where email = ? << TestUser03@gmail.com
+    Optional<User> findByEmail(String email);
 
+    // select * from user where account = ? and email = ?
+    Optional<User> findByAccountAndEmail(String account, String email);
 }
