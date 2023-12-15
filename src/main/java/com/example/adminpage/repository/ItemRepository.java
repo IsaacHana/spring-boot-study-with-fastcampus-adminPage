60a684042a12957091a696e3a3833340d1b1029d
Isaac Han <isaachan233@gmail.com>
2023-12-15 23:51:16 +0900

    @Repository 는 왜 작성하는가?

--- src/main/java/com/example/adminpage/repository/ItemRepository.java
+++ src/main/java/com/example/adminpage/repository/ItemRepository.java
@@ -4,6 +4,6 @@ import com.example.adminpage.model.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+// @Repository 없어도 동작 하는 지 실험
 public interface ItemRepository extends JpaRepository<Item, Long> {
 }
