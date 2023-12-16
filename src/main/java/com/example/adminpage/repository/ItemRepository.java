057afcc5183d880b6e3cb366f304eeed8c5ceefd
Isaac Han <isaachan233@gmail.com>
2023-12-16 22:24:10 +0900

    @Repository 명시적으로 달아주기

--- src/main/java/com/example/adminpage/repository/ItemRepository.java
+++ src/main/java/com/example/adminpage/repository/ItemRepository.java
@@ -4,6 +4,6 @@ import com.example.adminpage.model.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-// @Repository 없어도 동작 하는 지 실험
+@Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
 }
