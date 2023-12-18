074677813ad8ceb8756f2d874001efeafa7f0bd9
Isaac Han <isaachan233@gmail.com>
2023-12-18 20:41:09 +0900

    test: Jpa AuditorAware test

--- src/test/java/com/example/adminpage/repository/AdminUserRepositoryTest.java
+++ src/test/java/com/example/adminpage/repository/AdminUserRepositoryTest.java
@@ -3,10 +3,10 @@ package com.example.adminpage.repository;
 import com.example.adminpage.AdminPageApplicationTests;
 import com.example.adminpage.model.entity.AdminUser;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.time.LocalDateTime;
 import java.util.Optional;
 
 public class AdminUserRepositoryTest extends AdminPageApplicationTests {
@@ -21,13 +21,32 @@ public class AdminUserRepositoryTest extends AdminPageApplicationTests {
         adminUser.setPassword("AdminUser01");
         adminUser.setStatus("REGISTERED");
         adminUser.setRole("PARTNER");
-        adminUser.setCreatedAt(LocalDateTime.now());
-        adminUser.setCreatedBy("AdminServer");
+        // AuditorAware 적용 테스트
+//        adminUser.setCreatedAt(LocalDateTime.now());
+//        adminUser.setCreatedBy("AdminServer");
 
         AdminUser newAdminUser = adminUserRepository.save(adminUser);
         Assertions.assertNotNull(newAdminUser);
     }
 
+
+    @DisplayName("AuditorAware 적용 테스트")
+    @Test
+    public void createWithAuditorAware() {
+        AdminUser adminUser = new AdminUser();
+
+        adminUser.setAccount("AdminUser01");
+        adminUser.setPassword("AdminUser01");
+        adminUser.setStatus("REGISTERED");
+        adminUser.setRole("PARTNER");
+        // createdAt과 createdBy 은 auditorAware 에서 감시한다.
+
+        AdminUser newAdminUser = adminUserRepository.save(adminUser);
+        Assertions.assertNotNull(newAdminUser);
+
+        newAdminUser.setAccount("CHANGE");
+        adminUserRepository.save(newAdminUser);
+    }
     @Test
     public void read(){
         Optional<AdminUser> adminUser = adminUserRepository.findById(1L);
