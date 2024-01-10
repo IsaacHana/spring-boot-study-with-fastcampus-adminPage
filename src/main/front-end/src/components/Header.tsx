62a475adf36ee1ee190223a03c00f87c1857cb6d
IsaacHana <isaachan233@gmail.com>
2024-01-10 23:51:03 +0900

    header 꾸미기

--- src/main/front-end/src/components/Header.tsx
+++ src/main/front-end/src/components/Header.tsx
@@ -1,16 +1,21 @@
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 
 const Header = () => {
-  return (
-    <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-cyan-200 h-16">
-      <section>
-        <div className="flex flex-row gap-x-2">
-          <Link to="/">Admin</Link>
+    return (
+        <div
+            className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-primary h-16">
+            <section>
+                <div className="flex flex-row gap-x-2">
+                    <div className="flex flex-row">
+                        <Link to="/" className="text-2xl text-stone-200">
+                            Admin
+                        </Link>
+                    </div>
+                </div>
+            </section>
+            <div></div>
         </div>
-      </section>
-      <div>카테고리</div>
-    </div>
-  );
+    );
 };
 
 export default Header;
