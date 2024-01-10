8e44eedc6ce83fc007220175d629ffc035853c1c
IsaacHana <isaachan233@gmail.com>
2024-01-10 23:48:21 +0900

    확장 아이콘 & 목록 추가

--- src/main/front-end/src/components/Sidebar.tsx
+++ src/main/front-end/src/components/Sidebar.tsx
@@ -1,9 +1,16 @@
-import { Link, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { BsArrowBarRight } from "react-icons/bs";
 
 const Sidebar = () => {
   return (
-    <div className="space-y-4 flex flex-col h-full bg-slate-600">
-      <Link to="/user">고객관리</Link>
+    <div className="space-y-4 flex flex-col p-2 align-middle h-full bg-slate-600 text-stone-200 w-[60px]">
+      <div className="p-2 rounded-md transition-all hover:bg-slate-500">
+        <BsArrowBarRight size="28" />
+      </div>
+      <Link to="/user">고객 관리</Link>
+      <Link to="/item">물품 관리</Link>
+      <Link to="/order-group">주문 관리</Link>
+      <Link to="/partner">파트너사 관리</Link>
     </div>
   );
 };
