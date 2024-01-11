4985f675071a08fb5a6a725fda3f7b9af8e48c04
IsaacHana <isaachan233@gmail.com>
2024-01-11 22:47:03 +0900

    사이드바 css 추가 및 반응형 적용

--- src/main/front-end/src/components/Sidebar.tsx
+++ src/main/front-end/src/components/Sidebar.tsx
@@ -1,18 +1,38 @@
-import { Link } from "react-router-dom";
-import { BsArrowBarRight } from "react-icons/bs";
+import {Link} from "react-router-dom";
+import {BsArrowBarLeft, BsArrowBarRight} from "react-icons/bs";
 
-const Sidebar = () => {
-  return (
-    <div className="space-y-4 flex flex-col p-2 align-middle h-full bg-slate-600 text-stone-200 w-[60px]">
-      <div className="p-2 rounded-md transition-all hover:bg-slate-500">
-        <BsArrowBarRight size="28" />
-      </div>
-      <Link to="/user">고객 관리</Link>
-      <Link to="/item">물품 관리</Link>
-      <Link to="/order-group">주문 관리</Link>
-      <Link to="/partner">파트너사 관리</Link>
-    </div>
-  );
+interface SidebarProps {
+    isExpended: boolean;
+    onClickExpansion: () => void;
+}
+
+const Sidebar: React.FC<SidebarProps> = ({isExpended, onClickExpansion}) => {
+    return (
+        <div className="space-y-4 flex flex-col p-2 align-middle h-full bg-slate-900 text-stone-200">
+            <div className="flex flex-row justify-between align-middle">
+                <div
+                    className={`${isExpended ? "flex" : "hidden"} items-center text-xl`}
+                >
+                    Dash Board
+                </div>
+                <div
+                    onClick={onClickExpansion}
+                    className="hidden md:flex p-2 rounded-md transition-all hover:bg-slate-800"
+                >
+                    {" "}
+                    {isExpended ? (
+                        <BsArrowBarLeft size="28"/>
+                    ) : (
+                        <BsArrowBarRight size="28"/>
+                    )}
+                </div>
+            </div>
+            <Link to="/user">고객 관리</Link>
+            <Link to="/item">물품 관리</Link>
+            <Link to="/order-group">주문 관리</Link>
+            <Link to="/partner">파트너사 관리</Link>
+        </div>
+    );
 };
 
 export default Sidebar;
