a0af46145465b2bbe5d22ae81b353f09b03d728d
IsaacHana <isaachan233@gmail.com>
2024-01-11 23:06:25 +0900

    import 수정

--- src/main/front-end/src/App.tsx
+++ src/main/front-end/src/App.tsx
@@ -1,25 +1,24 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 import Home from "./home/home";
 import User from "./user/user";
 
-import { Route, Routes } from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import Layout from "./layout";
 import Items from "./items/items";
 
 function App() {
-  return (
-    <div className="App">
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/user" element={<User />} />
-          <Route path="/item" element={<Items />} />
-        </Routes>
-      </Layout>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Layout>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/user" element={<User/>}/>
+                    <Route path="/item" element={<Items/>}/>
+                </Routes>
+            </Layout>
+        </div>
+    );
 }
 
 export default App;
