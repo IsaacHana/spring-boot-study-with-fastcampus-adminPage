5880e1b1adbb27caf09ee1485d5e8663ac679b94
IsaacHana <isaachan233@gmail.com>
2024-01-12 17:48:49 +0900

    feat: table component 함수 작성

---
+++ src/main/front-end/src/components/Table.tsx
@@ -0,0 +1,44 @@
+interface TableProps<T> {
+  data: T[] | null;
+  keys: string[];
+  tableHeads: string[];
+}
+
+type TableComponentI<T = any> = React.FC<TableProps<T>>;
+
+const Table: TableComponentI = ({ data, keys, tableHeads }) => {
+  return (
+    <>
+      {data ? (
+        <>
+          <div className="relative overflow-x-auto shadow-md rounded-lg">
+            <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+              <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 rounded-lg">
+                <tr>
+                  {tableHeads.map((value) => (
+                    <th scope="col" className="px-6 py-3">
+                      {value}
+                    </th>
+                  ))}
+                </tr>
+              </thead>
+              <tbody>
+                {data.map((d) => (
+                  <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:opacity-80 transition-all hover:text-stone-200">
+                    {keys.map((key) => (
+                      <td className="text-middle px-6 py-4">{d[key]}</td>
+                    ))}
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </>
+      ) : (
+        <></>
+      )}
+    </>
+  );
+};
+
+export default Table;
