fbe0c1200822ab7ef79dcbecfc7e291458c8440f
IsaacHana <isaachan233@gmail.com>
2024-01-12 20:23:21 +0900

    feat: partner route 추가

--- src/main/front-end/src/App.tsx
+++ src/main/front-end/src/App.tsx
@@ -3,22 +3,24 @@ import "./App.css";
 import Home from "./home/home";
 import User from "./user/user";
 
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Layout from "./layout";
 import Items from "./items/items";
+import Partners from "./partners/partners";
 
 function App() {
-    return (
-        <div className="App">
-            <Layout>
-                <Routes>
-                    <Route path="/" element={<Home/>}/>
-                    <Route path="/user" element={<User/>}/>
-                    <Route path="/item" element={<Items/>}/>
-                </Routes>
-            </Layout>
-        </div>
-    );
+  return (
+    <div className="App">
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/user" element={<User />} />
+          <Route path="/item" element={<Items />} />
+          <Route path="/partner" element={<Partners />} />
+        </Routes>
+      </Layout>
+    </div>
+  );
 }
 
 export default App;
