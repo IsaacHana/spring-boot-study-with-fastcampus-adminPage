587ad26ff25c4be5773c8363ab027bc7c82a3fbc
IsaacHana <isaachan233@gmail.com>
2024-01-12 20:23:59 +0900

    layout css변경

--- src/main/front-end/src/layout.tsx
+++ src/main/front-end/src/layout.tsx
@@ -1,31 +1,35 @@
-import {useState} from "react";
+import { useState } from "react";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 
-const Layout = ({children}: { children: React.ReactNode }) => {
-    const [isExpended, setIsExpended] = useState<boolean>(false);
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  const [isExpended, setIsExpended] = useState<boolean>(false);
 
-    const handleExpendsion = () => {
-        setIsExpended((preProps) => {
-            return !preProps;
-        });
-    };
+  const handleExpendsion = () => {
+    setIsExpended((preProps) => {
+      return !preProps;
+    });
+  };
 
-    return (
-        <div className="h-full">
-            <Header/>
-            <div
-                className={`mt-16 ${
-                    isExpended ? "w-52" : "w-16"
-                } flex flex-col fixed inset-y-0`}
-            >
-                <Sidebar isExpended={isExpended} onClickExpansion={handleExpendsion}/>
-            </div>
-            <main className={`${isExpended ? "pl-52" : "pl-16 "} pt-16 h-full`}>
-                {children}
-            </main>
-        </div>
-    );
+  return (
+    <div className="h-full">
+      <Header />
+      <div
+        className={`mt-16 transition-all ${
+          isExpended ? "w-52" : "w-16"
+        } flex flex-col fixed inset-y-0`}
+      >
+        <Sidebar isExpended={isExpended} onClickExpansion={handleExpendsion} />
+      </div>
+      <main
+        className={`${
+          isExpended ? "pl-52" : "pl-16 "
+        } pt-16 h-full transition-all`}
+      >
+        {children}
+      </main>
+    </div>
+  );
 };
 
 export default Layout;
