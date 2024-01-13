92eb91867b83e8f90f420338f0a0b5226f93c520
IsaacHana <isaachan233@gmail.com>
2024-01-13 22:45:57 +0900

    feat: user 상세 페이지 api 연결

--- src/main/front-end/src/api/userApi.tsx
+++ src/main/front-end/src/api/userApi.tsx
@@ -3,13 +3,23 @@ import axios from "axios";
 const url = "/api/user";
 
 export const fetchUsers = async (currentPage: number, itemsPerPage: number) => {
-    const response = await axios.get(
-        `${url}?page=${currentPage}&size=${itemsPerPage}`
-    );
+  const response = await axios.get(
+    `${url}?page=${currentPage}&size=${itemsPerPage}`
+  );
 
-    if (response.status != 200) {
-        throw new Error(response.data?.description);
-    }
+  if (response.status != 200) {
+    throw new Error(response.data?.description);
+  }
 
-    return response.data;
+  return response.data;
+};
+
+export const fetchUser = async (userId: any) => {
+  const response = await axios.get(`${url}/${userId}`);
+
+  if (response.status != 200) {
+    throw new Error(response.data?.description);
+  }
+
+  return response.data;
 };
