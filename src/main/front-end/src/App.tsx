adeba54b3b77e54d9eb9eb843bd1b24bfe2501ee
IsaacHana <isaachan233@gmail.com>
2024-01-13 22:44:08 +0900

    코드 정리

--- src/main/front-end/src/App.tsx
+++ src/main/front-end/src/App.tsx
@@ -8,6 +8,7 @@ import Layout from "./layout";
 import Items from "./items/items";
 import Partners from "./partners/partners";
 import OrderGroup from "./orders/orders";
+import DetailUserPage from "./user/[userid]/user";
 
 function App() {
   return (
@@ -15,7 +16,10 @@ function App() {
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
+
           <Route path="/user" element={<User />} />
+          <Route path="/user/:id" element={<DetailUserPage />} />
+
           <Route path="/item" element={<Items />} />
           <Route path="/order-group" element={<OrderGroup />} />
           <Route path="/partner" element={<Partners />} />
