4246adae3ef886d979c781567bbe1a2a2d550d2b
IsaacHana <isaachan233@gmail.com>
2024-01-13 22:45:15 +0900

    feat: 상세 페이지 navigate 추가

--- src/main/front-end/src/components/Table.tsx
+++ src/main/front-end/src/components/Table.tsx
@@ -1,3 +1,5 @@
+import { Link, useLocation, useNavigate } from "react-router-dom";
+
 interface TableProps<T> {
   data: T[] | null;
   keys: string[];
@@ -7,6 +9,10 @@ interface TableProps<T> {
 type TableComponentI<T = any> = React.FC<TableProps<T>>;
 
 const Table: TableComponentI = ({ data, keys, tableHeads }) => {
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+  console.log(pathname);
+
   return (
     <>
       {data ? (
@@ -24,7 +30,13 @@ const Table: TableComponentI = ({ data, keys, tableHeads }) => {
               </thead>
               <tbody>
                 {data.map((d) => (
-                  <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:opacity-80 transition-all hover:text-stone-200">
+                  <tr
+                    key={d.id}
+                    onClick={() => {
+                      navigate(`${pathname}/${d.id}`);
+                    }}
+                    className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:opacity-80 transition-all hover:text-stone-200"
+                  >
                     {keys.map((key) => (
                       <td className="text-middle px-6 py-4">{d[key]}</td>
                     ))}
