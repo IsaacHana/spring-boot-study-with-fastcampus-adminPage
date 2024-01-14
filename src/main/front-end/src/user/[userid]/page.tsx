85e124a0f9fd22ac44ad03ad62f093d7b425328c
IsaacHana <isaachan233@gmail.com>
2024-01-14 19:34:03 +0900

    feat: react-hook-form 적용하여 update 적용

--- src/main/front-end/src/user/[userid]/page.tsx
+++ src/main/front-end/src/user/[userid]/page.tsx
@@ -1,10 +1,13 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 
 import { User } from "../../model/model";
 import ErrorPage from "../../components/ErrorPage";
-import { fetchUser } from "../../api/userApi";
-import { useParams } from "react-router-dom";
+import { fetchUser, updateUser } from "../../api/userApi";
+import { useLocation, useParams } from "react-router-dom";
 import Loader from "../../components/Loader";
+import Input from "../../ui/Input";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import axios from "axios";
 
 const Page = () => {
   const { id } = useParams();
@@ -14,8 +17,38 @@ const Page = () => {
 
   const [user, setUser] = useState<User | null>(null);
 
+  const {
+    register,
+    reset,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: useMemo(() => {
+      return {
+        status: user?.status,
+        account: user?.account,
+        password: user?.password,
+        email: user?.email,
+        phone_number: user?.phone_number,
+        registered_at: user?.registered_at,
+        unregistered_at: user?.unregistered_at,
+      };
+    }, [user]),
+  });
+
   useEffect(() => {
-    console.log(id);
+    reset({
+      status: user?.status,
+      account: user?.account,
+      password: user?.password,
+      email: user?.email,
+      phone_number: user?.phone_number,
+      registered_at: user?.registered_at,
+      unregistered_at: user?.unregistered_at,
+    });
+  }, [user]);
+
+  useEffect(() => {
     const fetchData = async () => {
       setIsFetching(true);
       try {
@@ -59,17 +92,102 @@ const Page = () => {
     );
   }
 
+  const onSubmit: SubmitHandler<FieldValues> = (updatedData) => {
+    console.log(id);
+    const updateData = async () => {
+      setIsFetching(true);
+
+      try {
+        const { data } = await updateUser({ ...updatedData, id });
+
+        setUser(data);
+
+        setIsFetching(false);
+      } catch (error: unknown) {
+        if (error instanceof Error) {
+          console.log(error);
+          setError({
+            ...error,
+            message: error.message || "Could not update user",
+          });
+        }
+      }
+    };
+    updateData();
+  };
   return (
     <>
-      <div className="grid grid-cols-2 text-stone-200">
-        <div className="">{user.account}</div>
-        <div>{user.status}</div>
-        <div>{user.email}</div>
-        <div>{user.id}</div>
-        <div>{user.password}</div>
-        <div>{user.phone_number}</div>
-        <div>{user.registered_at}</div>
-        {user.status === "REGISTERED" && <div>{user.unregistered_at}</div>}
+      <div className="p-4 m-12">
+        <div className="grid grid-cols-2 gap-6">
+          <Input
+            id="status"
+            label="상태"
+            disabled={isFetching}
+            register={register}
+            errors={errors}
+            required
+          />
+          <Input
+            id="account"
+            label="계정"
+            disabled={isFetching}
+            register={register}
+            errors={errors}
+            required
+          />
+          <Input
+            id="password"
+            label="비밀번호"
+            disabled={isFetching}
+            register={register}
+            errors={errors}
+            required
+          />
+          <Input
+            id="email"
+            label="이메일"
+            pattern={/^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i}
+            disabled={isFetching}
+            register={register}
+            errors={errors}
+            required
+          />
+          <Input
+            id="phone_number"
+            label="전화번호"
+            disabled={isFetching}
+            register={register}
+            errors={errors}
+            required
+          />
+          <Input
+            id="registered_at"
+            label="등록일"
+            disabled={isFetching}
+            register={register}
+            errors={errors}
+            required
+          />
+          {user.status !== "REGISTERED" && (
+            <Input
+              id="unregistered_at"
+              label="해지일"
+              disabled={isFetching}
+              register={register}
+              errors={errors}
+              required
+            />
+          )}
+        </div>
+
+        <div className="flex justify-end mt-4">
+          <button
+            className="bg-lime-600 text-stone-200 w-[60px] h-[40px] rounded-lg"
+            onClick={handleSubmit(onSubmit)}
+          >
+            등록
+          </button>
+        </div>
       </div>
     </>
   );
