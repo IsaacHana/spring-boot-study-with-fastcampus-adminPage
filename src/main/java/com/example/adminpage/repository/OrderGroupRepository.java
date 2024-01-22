9d7fc67312ed26ca5f8e53b9c71672cda8b52443
IsaacHana <isaachan233@gmail.com>
2024-01-23 02:11:01 +0900

    refactor: orderGroup fetch join 적용해보기

--- src/main/java/com/example/adminpage/repository/OrderGroupRepository.java
+++ src/main/java/com/example/adminpage/repository/OrderGroupRepository.java
@@ -1,9 +1,14 @@
 package com.example.adminpage.repository;
 
 import com.example.adminpage.model.entity.OrderGroup;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OrderGroupRepository extends JpaRepository<OrderGroup, Long> {
+    @Query("SELECT og from OrderGroup og JOIN FETCH og.user u where u.id = ?1")
+    Page<OrderGroup> findWithUserId(Long id, Pageable pageable);
 }
