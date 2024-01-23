771f7e1a65171936a0b4eaa5a35530786aed7d23
IsaacHana <isaachan233@gmail.com>
2024-01-23 17:56:17 +0900

    style: 로고 이름 text-2xl -> text-3xl 변경

--- src/main/front-end/src/components/Header.tsx
+++ src/main/front-end/src/components/Header.tsx
@@ -1,21 +1,20 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-    return (
-        <div
-            className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-primary h-16">
-            <section>
-                <div className="flex flex-row gap-x-2">
-                    <div className="flex flex-row">
-                        <Link to="/" className="text-2xl text-stone-200">
-                            Admin
-                        </Link>
-                    </div>
-                </div>
-            </section>
-            <div></div>
+  return (
+    <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-primary h-16">
+      <section>
+        <div className="flex flex-row gap-x-2">
+          <div className="flex flex-row ">
+            <Link to="/" className="text-3xl text-stone-200">
+              Admin
+            </Link>
+          </div>
         </div>
-    );
+      </section>
+      <div></div>
+    </div>
+  );
 };
 
 export default Header;
