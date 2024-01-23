99aab99dc0994607ed80eff378f62dcd573713e4
IsaacHana <isaachan233@gmail.com>
2024-01-23 22:12:18 +0900

    feat: OrderGroup Api fetch, update, delete 추가

--- src/main/front-end/src/api/orderGroupApi.tsx
+++ src/main/front-end/src/api/orderGroupApi.tsx
@@ -1,14 +1,45 @@
 import axios from "axios";
+import { FieldValues } from "react-hook-form";
 
 const url = "/api/order-group";
 
-export const fetchOrderGroup = async (
+export const fetchOrderGroups = async (
   currentPage: number,
   orderGroupPerPage: number
 ) => {
   const response = await axios.get(
     `${url}?page=${currentPage}&size=${orderGroupPerPage}`
   );
+
+  if (response.status != 200) {
+    throw new Error(response.data?.description);
+  }
+
+  return response.data;
+};
+
+export const fetchOrderGroup = async (id: string) => {
+  const response = await axios.get(`${url}/${id}`);
+
+  if (response.status != 200) {
+    throw new Error(response.data?.description);
+  }
+
+  return response.data;
+};
+
+export const updateOrderGroup = async (updatedData: FieldValues) => {
+  const response = await axios.put(`${url}`, updatedData);
+
+  if (response.status != 200) {
+    throw new Error(response.data?.description);
+  }
+
+  return response.data;
+};
+
+export const deleteOrderGroup = async (id: number) => {
+  const response = await axios.delete(`${url}/${id}`);
 
   if (response.status != 200) {
     throw new Error(response.data?.description);
