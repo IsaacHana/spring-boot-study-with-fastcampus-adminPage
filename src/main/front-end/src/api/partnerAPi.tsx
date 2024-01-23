9086280b9ef5f96ec83bee26afead975ff45c2b4
IsaacHana <isaachan233@gmail.com>
2024-01-23 22:13:44 +0900

    partner Api fetch, update, delete 추가

--- src/main/front-end/src/api/partnerAPi.tsx
+++ src/main/front-end/src/api/partnerAPi.tsx
@@ -1,14 +1,47 @@
 import axios from "axios";
+import { FieldValues } from "react-hook-form";
 
 const url = "/api/partner";
 
-export const fetchPartner = async (
+export const fetchPartners = async (
   currentPage: number,
   itemsPerPage: number
 ) => {
   const response = await axios.get(
     `${url}?page=${currentPage}&size=${itemsPerPage}`
   );
+
+  if (response.status != 200) {
+    throw new Error(response.data?.description);
+  }
+
+  return response.data;
+};
+
+export const fetchPartner = async (id: string) => {
+  const response = await axios.get(`${url}/${id}`);
+
+  if (response.status != 200) {
+    throw new Error(response.data?.description);
+  }
+
+  console.log(response.data);
+
+  return response.data;
+};
+
+export const updatePartner = async (updatedData: FieldValues) => {
+  const response = await axios.put(`${url}`, updatedData);
+
+  if (response.status != 200) {
+    throw new Error(response.data?.description);
+  }
+
+  return response.data;
+};
+
+export const deletePartner = async (id: number) => {
+  const response = await axios.delete(`${url}/${id}`);
 
   if (response.status != 200) {
     throw new Error(response.data?.description);
