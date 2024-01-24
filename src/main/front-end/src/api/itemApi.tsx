9882af64a1a7dfe5fa1eca8a42a6595641877e25
IsaacHana <isaachan233@gmail.com>
2024-01-24 19:33:02 +0900

    feat: item api 연결 추가

--- src/main/front-end/src/api/itemApi.tsx
+++ src/main/front-end/src/api/itemApi.tsx
@@ -1,15 +1,46 @@
 import axios from "axios";
+import { FieldValues } from "react-hook-form";
 
 const url = "/api/item";
 
 export const fetchItems = async (currentPage: number, itemsPerPage: number) => {
-    const response = await axios.get(
-        `${url}?page=${currentPage}&size=${itemsPerPage}`
-    );
+  const response = await axios.get(
+    `${url}?page=${currentPage}&size=${itemsPerPage}`
+  );
 
-    if (response.status != 200) {
-        throw new Error(response.data?.description);
-    }
+  if (response.status != 200) {
+    throw new Error(response.data?.description);
+  }
 
-    return response.data;
+  return response.data;
+};
+
+export const fetchItem = async (id: string) => {
+  const response = await axios.get(`${url}/${id}`);
+
+  if (response.status != 200) {
+    throw new Error(response.data?.description);
+  }
+
+  return response.data;
+};
+
+export const updatedItem = async (updatedData: FieldValues) => {
+  const response = await axios.put(`${url}`, updatedData);
+
+  if (response.status != 200) {
+    throw new Error(response.data?.description);
+  }
+
+  return response.data;
+};
+
+export const deleteItem = async (id: number) => {
+  const response = await axios.delete(`${url}/${id}`);
+
+  if (response.status != 200) {
+    throw new Error(response.data?.description);
+  }
+
+  return response.data;
 };
