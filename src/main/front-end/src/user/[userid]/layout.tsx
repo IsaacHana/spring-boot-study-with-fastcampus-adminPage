9115662ebdaadfe7da962a20fdbfbac35abe2ef9
IsaacHana <isaachan233@gmail.com>
2024-01-24 19:33:28 +0900

    style: 레이아웃 패딩 추기

--- src/main/front-end/src/user/[userid]/layout.tsx
+++ src/main/front-end/src/user/[userid]/layout.tsx
@@ -1,5 +1,5 @@
 const DetailUserPageLayout = ({ children }: { children: React.ReactNode }) => {
-  return <>{children}</>;
+  return <div className="p-8">{children}</div>;
 };
 
 export default DetailUserPageLayout;
