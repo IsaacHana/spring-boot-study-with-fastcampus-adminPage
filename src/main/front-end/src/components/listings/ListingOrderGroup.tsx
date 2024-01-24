dc144385b4862d4f508d658f32eeed7b56502da9
IsaacHana <isaachan233@gmail.com>
2024-01-24 19:37:13 +0900

    style: 상세 주문 정보 재배치

--- src/main/front-end/src/components/listings/ListingOrderGroup.tsx
+++ src/main/front-end/src/components/listings/ListingOrderGroup.tsx
@@ -19,59 +19,64 @@ const ListingOrderGroup: React.FC<ListingOrderGroupProps> = ({
             {new Date(data.order_at).toLocaleDateString()} 주문
           </span>
         </div>
-        {data.order_detail_api_responses.map((orderDetail) => (
-          <div
-            key={orderDetail.id}
-            className="flex flex-row my-4 w-full flex-wrap"
-          >
-            <div className="flex flex-col flex-[3] p-4 rounded-l-lg border border-stone-200 border-opacity-30">
-              {orderDetail.item_api_response && (
-                <>
-                  <div className="flex flex-row">
-                    <span className="font-bold text-xl mr-4">
-                      {orderDetail.status}
-                    </span>
-                    {orderDetail.status !== "ORDERING" && (
-                      <span className="font-light text-xl text-indigo-200">
-                        {new Date(
-                          orderDetail.arrival_date
-                        ).toLocaleDateString()}
+        {data.order_detail_api_responses &&
+          data.order_detail_api_responses.map((orderDetail) => (
+            <div
+              key={orderDetail.id}
+              className="flex flex-row my-4 w-full flex-wrap"
+            >
+              <div className="flex flex-col flex-[3] p-4 rounded-l-lg border border-stone-200 border-opacity-30">
+                {orderDetail.item_api_response && (
+                  <>
+                    <div className="flex flex-row">
+                      <span className="font-bold text-xl mr-4">
+                        {orderDetail.status}
                       </span>
-                    )}
-                  </div>
-                  <div className="flex flex-col pt-4 md:flex-row">
-                    <img
-                      src={favicon}
-                      className="border rounded-lg object-cover h-40 w-60 mr-4"
-                    />
-                    <div
-                      key={orderDetail.item_api_response.id}
-                      className="flex gap-4"
-                    >
-                      <div>
-                        <div>
-                          {orderDetail.item_api_response.title},
-                          {orderDetail.item_api_response.name},
-                        </div>
-                        <div className="flex">
-                          <span className="text-xl">
-                            {orderDetail.item_api_response.price} {" 원 · "}
-                            {orderDetail.quantity || "0 개"}
-                          </span>
+                      {orderDetail.status !== "ORDERING" && (
+                        <span className="font-light text-xl text-indigo-200">
+                          {new Date(
+                            orderDetail.arrival_date
+                          ).toLocaleDateString()}
+                        </span>
+                      )}
+                    </div>
+                    <div className="flex flex-col pt-4 md:flex-row">
+                      <img
+                        src={favicon}
+                        className="border rounded-lg object-cover h-40 w-60 mr-4"
+                      />
+                      <div
+                        key={orderDetail.item_api_response.id}
+                        className="flex gap-4"
+                      >
+                        <div className="flex flex-col text-start">
+                          <p className="text-2xl">
+                            {`${orderDetail.item_api_response.title}, ${orderDetail.item_api_response.name}`}
+                          </p>
+
+                          <p className="text-xl">
+                            {`${orderDetail.item_api_response.price} 원 · ${
+                              orderDetail.quantity || 0
+                            } 개`}
+                          </p>
+                          <p className="text-xl">{`총 ${orderDetail.total_price} 원`}</p>
                         </div>
                       </div>
                     </div>
-                  </div>
-                </>
-              )}
-            </div>
+                  </>
+                )}
+              </div>
 
-            <div className="flex flex-col flex-[1] flex-wrap justify-between p-4 rounded-r-lg border border-stone-200 border-opacity-30">
-              <div className="border border-stone-200 rounded-md p-2">수정</div>
-              <div className="border border-stone-200 rounded-md p-2">삭제</div>
+              <div className="flex flex-col flex-[1] flex-wrap justify-between p-4 rounded-r-lg border border-stone-200 border-opacity-30">
+                <div className="border border-stone-200 rounded-md p-2">
+                  수정
+                </div>
+                <div className="border border-stone-200 rounded-md p-2">
+                  삭제
+                </div>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
       </div>
     </>
   );
