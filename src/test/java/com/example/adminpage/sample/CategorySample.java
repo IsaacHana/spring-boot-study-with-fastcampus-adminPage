028b8374a1e610292533cee7d7012dfdd5fcb354
IsaacHana <isaachan233@gmail.com>
2024-01-30 23:22:57 +0900

    sample: category sample data 수정

--- src/test/java/com/example/adminpage/sample/CategorySample.java
+++ src/test/java/com/example/adminpage/sample/CategorySample.java
@@ -3,6 +3,7 @@ package com.example.adminpage.sample;
 import com.example.adminpage.component.LoginUserAuditorAware;
 import com.example.adminpage.config.JpaConfig;
 import com.example.adminpage.model.entity.Category;
+import com.example.adminpage.model.enumclass.CategoryType;
 import com.example.adminpage.repository.CategoryRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,9 +13,6 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Arrays;
-import java.util.List;
 
 @DataJpaTest                                                                   // JPA 테스트 관련 컴포넌트만 Import
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)    // 실제 db 사용
@@ -28,12 +26,11 @@ public class CategorySample {
 
     @Test
     public void createSample() {
-        List<String> category = Arrays.asList("COMPUTER", "CLOTHING", "MULTI_SHOP", "INTERIOR", "FOOD", "SPORTS", "SHOPPING_MALL", "DUTY_FREE", "BEAUTY");
-        List<String> title = Arrays.asList("컴퓨터-전자제품", "의류", "멀티샵", "인테리어", "음식", "스포츠", "쇼핑몰", "면세점", "화장");
+        CategoryType[] category = CategoryType.values();
 
-        for (int i = 0; i < category.size(); i++) {
-            String c = category.get(i);
-            String t = title.get(i);
+        for (int i = 0; i < category.length; i++) {
+            CategoryType c = category[i];
+            String t = c.getName();
             Category create = Category.builder().type(c).title(t).build();
             categoryRepository.save(create);
         }
