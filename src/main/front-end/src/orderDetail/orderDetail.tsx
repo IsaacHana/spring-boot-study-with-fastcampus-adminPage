740d94616d2e1ba1d494a2abe355f2f4d04fe704
IsaacHana <isaachan233@gmail.com>
2024-01-31 15:25:59 +0900

    feat: order detail page 추가

---
+++ src/main/front-end/src/orderDetail/orderDetail.tsx
@@ -0,0 +1,12 @@
+import OrderGroupsLayout from "./layout";
+import Page from "./page";
+
+const OrderDetail = () => {
+  return (
+    <OrderGroupsLayout>
+      <Page />
+    </OrderGroupsLayout>
+  );
+};
+
+export default OrderDetail;
